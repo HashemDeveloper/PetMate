fb930c21c70bff5f9816a93eb42ac0e8721af216
Abul Hashem Jr <abul@limosys.com>
2020-02-20 16:12:20 -0500

    Initial setup

---
+++ app/src/main/java/com/project/petmate/PetMateMainActivity.kt
@@ -0,0 +1,12 @@
+package com.project.petmate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PetMateMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
